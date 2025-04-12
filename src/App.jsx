d800171bbeff66db00203c6e47aa8ee044a1c943
Prajitha <prajitham2003@gmail.com>
2025-04-12 22:34:47 +0530

    new folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import SideBar from './components/SideBar'
 import courses from './data/courses'
@@ -11,7 +9,7 @@ function App() {
   }
   return (
     <>
-      <Main/>
+      <Main />
       <SideBar courses={courses} onclick={handleSelectCourse} />
     </>
   )
